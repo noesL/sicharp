64cc7c9ebf18bea979763e0bf2d9e37abae73de1
noesL <pecotovar25@gmail.com>
2022-09-13 15:17:33 -0500

    Contexto de algunas funcionalidades de los modulos de unity

--- C#/Scripts Unity/ControladorBasico.cs
+++ C#/Scripts Unity/ControladorBasico.cs
@@ -3,6 +3,15 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
+/* Script basico de controlador de un objeto Prefab, en este caso controla inputs >> outputs de movimiento de un jugador, 
+ * en unity hay modulos integrados de fisica que permiten crear colision y fuerzas reales como gravedad e impulsos (ver linea 13 y 23)
+ * Funciones Integradas de UNITY:
+ * START > Se inicializa en la frame 1 de modo de juego
+ * AWAKE > Se inicializa en la frame 0 (optima para renderear y cargar objetos (ver linea 20))
+ * UPDATE > Se reinicia y ejecuta cada una de las frames mientras el juego esta corriendo.
+ */
+
+
 public class PlayerController : MonoBehaviour
 {
 
@@ -12,11 +21,22 @@ public class PlayerController : MonoBehaviour
 
     void Awake()
     {
+        // Using gameObject.GetComponent will return the first component that is found and the order is undefined.
+        // If you expect there to be more than one component of the same type, use gameObject.GetComponents instead,
+        // and cycle through the returned components testing for some unique property.
         playerRigidBody = GetComponent<Rigidbody2D>();
     }
 
     void Jump() 
     {
+        // Apply a force to the rigidbody.
+
+        //The force is specified as two separate components in the X and Y directions(there is no Z direction in 2D physics).
+        //The object will be accelerated by the force according to the law force = mass x acceleration -the larger the mass,
+        //greater the force required to accelerate to a given speed.
+
+        // VECTOR2 INDICA MOVIMIENTO EN 2D
+        //IMPULSE: This mode is useful for applying forces that happen instantly, such as forces from explosions or collisions.
         playerRigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 
