528e2fd31951ecd91278f730e324d23145bb12de
noesL <pecotovar25@gmail.com>
2022-09-13 15:58:29 -0500

    01 Cambios a Script ControladorBasico

--- C#/Scripts Unity/ControladorBasico.cs
+++ C#/Scripts Unity/ControladorBasico.cs
@@ -4,10 +4,10 @@ using Unity.VisualScripting;
 using UnityEngine;
 
 /* Script basico de controlador de un objeto Prefab, en este caso controla inputs >> outputs de movimiento de un jugador, 
- * en unity hay modulos integrados de fisica que permiten crear colision y fuerzas reales como gravedad e impulsos (ver linea 13 y 23)
+ * en unity hay modulos integrados de fisica que permiten crear colision y fuerzas reales como gravedad e impulsos (ver lineas 19, 49, y 59)
  * Funciones Integradas de UNITY:
  * START > Se inicializa en la frame 1 de modo de juego
- * AWAKE > Se inicializa en la frame 0 (optima para renderear y cargar objetos (ver linea 20))
+ * AWAKE > Se inicializa en la frame 0 (optima para renderear y cargar objetos (ver linea 25))
  * UPDATE > Se reinicia y ejecuta cada una de las frames mientras el juego esta corriendo.
  */
 
@@ -18,6 +18,9 @@ public class PlayerController : MonoBehaviour
     //Character movement variables
     public float jumpForce = 6f;
     Rigidbody2D playerRigidBody;
+
+    //Capa que se va a ocupar de registrar que forma parte del suelo
+    public LayerMask groundMask;
 
     void Awake()
     {
@@ -37,7 +40,30 @@ public class PlayerController : MonoBehaviour
 
         // VECTOR2 INDICA MOVIMIENTO EN 2D
         //IMPULSE: This mode is useful for applying forces that happen instantly, such as forces from explosions or collisions.
-        playerRigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        
+        // Este metodo raycast permite al personaje saltar unicamente cuando este a una cierta
+        // distancia del suelo (evita moonjump)
+
+        if (isTouchingGround() == true)
+        {
+            playerRigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    //Method for recognizing if the player is touching the ground.
+    bool isTouchingGround()
+    {
+        //RAYCAST >> TRAZAR UN RAYO
+        //vector2D origin, vector2D direction, DISTANCIA (DESDE EL CENTRO DEL OBJETO, 1.5f = 1.5m, contra OBJETO)
+        //this.transform.position >> desde la posicion actual del objeto
+        if (Physics2D.Raycast(this.transform.position, Vector2.down, 1.5f, groundMask))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
     // Start is called before the first frame update
@@ -49,6 +75,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        // Operator || means or
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
         {
             Jump();
